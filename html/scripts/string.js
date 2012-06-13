602ebb7269be198f5547d53a5510c1841e94094a
Marc Matteo <marcm@lectroid.net>
2012-06-13 00:04:56 +0000

    More with the pattern matching.

--- html/scripts/string.js
+++ html/scripts/string.js
@@ -9,6 +9,9 @@ String.prototype.dequote = function() {
 String.prototype.decopify = function() {
   var text = this.dequote();
 
+  text = text.replace(/ \/ /, ' at ');
+  text = text.replace(/^(\w+\d+ \w) at \1/i, '$1 at');
+
   text = text.replace(/\bJNO\b/i, 'just north of');
   text = text.replace(/\bJSO\b/i, 'just south of');
   text = text.replace(/\bJEO\b/i, 'just east of');
@@ -29,16 +32,13 @@ String.prototype.decopify = function() {
 
   text = text.replace(/\bSR51\b/i, 'CAP CITY FWY');
 
-  text = text.replace(/ \/ /, ' at ');
   text = text.replace(/\bTrfc\b/i, 'Traffic');
   text = text.replace(/\bInj\b/i, 'Injury');
   text = text.replace(/\bEnrt\b/i, 'Enroute');
   text = text.replace(/\bVeh\b/ig, 'Vehicle');
   text = text.replace(/\bVehs\b/ig, 'Vehicles');
   text = text.replace(/\bUnk(n*)\b/i, 'Unknown');
-
   text = text.replace(/\b1141\b/, 'Ambulance');
-
   text = text.replace(/\bRP\b/i, 'reporting party');
   text = text.replace(/\bTC\b/i, 'collision');
   text = text.replace(/\bRHS\b/i, 'right hand side');
@@ -47,8 +47,6 @@ String.prototype.decopify = function() {
   text = text.replace(/\bRDWY\b/ig, 'roadway');
   text = text.replace(/\bINVLD\b/i, 'involved');
   text = text.replace(/\bCT\b/ig, 'CalTrans');
-
-  text = text.replace(/^(\w+\d+ \w) at \1/i, '$1 at');
 
   text = text.replace(/^FIRE-Report of$/, 'Report of Fire');
   text = text.replace(/Collision-(\w+)/, 'Collision - $1');
